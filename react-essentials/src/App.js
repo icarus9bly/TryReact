999fe31c19f3facd6014fb11f0777a2b7abf1860
Aditya Pratap Singh <adinick333@gmail.com>
2021-07-03 04:06:05 +0530

    useeffect and usestate

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,15 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+//Our Header component
 function Header(props) {
   return <header>Awesome header yo {props.className}🔥</header>;
 }
+//Our Section component
 function Section(props) {
   return (
     <section>
       <p>This in my Awesome main Area is for doing {props.content} 🥸</p>
       <img src="https://github.com/icarus9bly.png" height={400}></img>
       <ul style={{ textAlign: "left" }}>
+        {/* This is how to you do comments in JSX
+        Mapping over dishes prop and returning <li>
+        */}
         {props.dishes.map((dish) => (
           <li key={dish.id}>{dish.title}</li>
         ))}
@@ -17,10 +22,13 @@ function Section(props) {
     </section>
   );
 }
+//Our Footer component
 function Footer() {
   return <footer>Footer stuff 🥸🤣</footer>;
 }
+// Some dishes array
 const dishes = ["Atta Maggi", "Green Maggie", "Gazab Noodles"];
+// mapping dishes array to hold id in json object(aka Python dict😁) to avoid react key warning
 const dishObject = dishes.map((dish, i) => {
   return { id: i, title: dish };
 });
--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -1,21 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.js";
 import reportWebVitals from "./reportWebVitals";
 
+// A different component
 function NextCompi() {
-  return <h1>Kuch naya karte haen</h1>;
+  {
+    /* This is array destructering 
+    useState returns a pair of values: the current state and a function that updates it. This is why we write const [count, setCount] = useState(). This is similar to this.state.count and this.setState in a class, except you get them in a pair.
+    useEffect is to watch a change and it's second dependancy array is to only watch a current emotion.
+    */
+  }
+  const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
+  useEffect(() => {
+    {
+      console.log(`It's my current ${emotion} emotion`);
+    }
+  }, [emotion]);
+  useEffect(() => {
+    console.log(`It's my secondary ${secondary} emotion`);
+  }, [secondary]);
+  return (
+    <>
+      <h1>
+        Kuch naya karte haen with {emotion} emotion aur dosra emotion{" "}
+        {secondary}
+      </h1>
+      <button onClick={() => setEmotion("Super Happy")}>Happy🤪</button>
+      <button onClick={() => setEmotion("Un Happy")}>Un Happy😭</button>
+      <button onClick={() => setSecondary("Bhot ye jyda Happy")}>
+        Bhot Happy🤪
+      </button>
+      <button onClick={() => setSecondary("Ultra Happy")}>Ultra Happy😭</button>
+    </>
+  );
 }
+//If/else JSX ternary clause which returns either the App2 or NextCompi component based on authorized value from prop.
 function App2({ authorized }) {
   return (
     <>{authorized ? <h1>This is my second App Bufkins</h1> : <NextCompi />}</>
   );
 }
+// Use <> short for <React.Fragment> for a component to return multiple elements.
 ReactDOM.render(
   <>
     <App />
-    <App2 authorized={true} />
+    <App2 authorized={false} />
   </>,
   document.getElementById("root")
 );
